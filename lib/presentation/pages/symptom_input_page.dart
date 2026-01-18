8f295cbf1d44d49c382406ea5f308031cda86def
ashu13579 <71175730+ashu13579@users.noreply.github.com>
2026-01-19 01:40:25 +0530

    fix: Add explicit import for BodyRegion extension

--- lib/presentation/pages/symptom_input_page.dart
+++ lib/presentation/pages/symptom_input_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:symptom_checker/data/models/symptom_data.dart';
 import 'package:symptom_checker/presentation/bloc/symptom_bloc.dart';
+import 'package:symptom_checker/core/constants/body_regions.dart';
 
 class SymptomInputPage extends StatefulWidget {
   const SymptomInputPage({super.key});
