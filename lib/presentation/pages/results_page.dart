5dcad22a88bef53c398804491ff884f4f7ec25fb
ashu13579 <71175730+ashu13579@users.noreply.github.com>
2026-01-19 02:12:38 +0530

    fix: Add missing import for body_regions to access displayName extension

--- lib/presentation/pages/results_page.dart
+++ lib/presentation/pages/results_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:symptom_checker/presentation/bloc/symptom_bloc.dart';
 import 'package:symptom_checker/core/theme/app_theme.dart';
+import 'package:symptom_checker/core/constants/body_regions.dart';
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({super.key});
